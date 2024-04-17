420e65be2918c5411df45229c899a382261c072c
NetNinjaEngineer <memo5260287@outlook.com>
2024-04-18 00:01:21 +0200

    added weather model

---
+++ lib/Models/weather_model.dart
@@ -0,0 +1,29 @@
+class WeatherModel {
+  final String cityName;
+  final DateTime date;
+  final String image;
+  final double temp;
+  final double maxTemp;
+  final double minTemp;
+  final String weatherCondtion;
+
+  const WeatherModel(
+      {required this.cityName,
+      required this.date,
+      required this.image,
+      required this.temp,
+      required this.maxTemp,
+      required this.minTemp,
+      required this.weatherCondtion});
+  factory WeatherModel.fromJson(json) {
+    return WeatherModel(
+        cityName: json['location']['name'],
+        date: DateTime.parse(json['current']['last_updated']),
+        image: json['forecast']['forecastday'][0]['day']['condition']['icon'],
+        temp: json['forecast']['forecastday'][0]['day']['avgtemp_c'],
+        maxTemp: json['forecast']['forecastday'][0]['day']['maxtemp_c'],
+        minTemp: json['forecast']['forecastday'][0]['day']['mintemp_c'],
+        weatherCondtion: json['forecast']['forecastday'][0]['day']['condition']
+            ['text']);
+  }
+}
