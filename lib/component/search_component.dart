fa0e37633fcd310136fe8472737f044c6399c6fc
mostafa abdo <mostafamedhatabdo23@gmail.com>
2024-04-18 00:10:17 +0200

    added search and weather file

---
+++ lib/component/search_component.dart
@@ -0,0 +1,36 @@
+import 'package:copy_weather_app/cubit/get_current_cubit/get_current_cubit.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+class SearchComponent extends StatelessWidget {
+  const SearchComponent({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 12),
+          child: TextField(
+            onSubmitted: (value) async {
+              var weather_model = BlocProvider.of<GetCurrentCubit>(context);
+              weather_model.getCurrentWeather(cityName: value);
+              Navigator.pop(context);
+            },
+            decoration: InputDecoration(
+              suffixIcon: const Icon(Icons.search),
+              suffixIconColor: Colors.black,
+              contentPadding:
+                  const EdgeInsets.symmetric(horizontal: 16, vertical: 32),
+              labelText: 'Search',
+              hintText: 'Enter City Name',
+              border: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(10),
+                  borderSide: const BorderSide(color: Colors.green)),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/component/weather_component.dart
@@ -0,0 +1,71 @@
+import 'package:copy_weather_app/cubit/get_current_cubit/get_current_cubit.dart';
+import 'package:copy_weather_app/main.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+class WeatherComponent extends StatelessWidget {
+  const WeatherComponent({super.key});
+  @override
+  Widget build(BuildContext context) {
+    var models = BlocProvider.of<GetCurrentCubit>(context).weatherModel!;
+    return Container(
+      decoration: BoxDecoration(
+          gradient: LinearGradient(colors: [
+        getThemeColor(models.weatherCondtion),
+        getThemeColor(models.weatherCondtion)[300]!,
+        getThemeColor(models.weatherCondtion)[50]!,
+      ], begin: Alignment.topCenter, end: Alignment.bottomCenter)),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Text(models.cityName,
+              style:
+                  const TextStyle(fontSize: 26, fontWeight: FontWeight.bold)),
+          Text('updated at: ${models.date.hour}:${models.date.minute}',
+              style:
+                  const TextStyle(fontSize: 20, fontWeight: FontWeight.bold)),
+          const SizedBox(
+            height: 50,
+          ),
+          Row(
+            children: [
+              const Spacer(
+                flex: 1,
+              ),
+              Image.network(models.image.contains('https:')
+                  ? models.image
+                  : 'https:${models.image}'),
+              const Spacer(
+                flex: 1,
+              ),
+              Text(models.temp.round().toString(),
+                  style: const TextStyle(
+                      fontSize: 25, fontWeight: FontWeight.bold)),
+              const Spacer(
+                flex: 1,
+              ),
+              Column(
+                children: [
+                  Text('maxTemp: ${models.maxTemp.round()}',
+                      style: const TextStyle(
+                          fontSize: 16, fontWeight: FontWeight.bold)),
+                  Text('minTemp: ${models.minTemp.round()}',
+                      style: const TextStyle(
+                          fontSize: 16, fontWeight: FontWeight.bold)),
+                ],
+              ),
+              const Spacer(
+                flex: 1,
+              ),
+            ],
+          ),
+          const SizedBox(
+            height: 30,
+          ),
+          Text(models.weatherCondtion,
+              style: const TextStyle(fontSize: 26, fontWeight: FontWeight.bold))
+        ],
+      ),
+    );
+  }
+}
